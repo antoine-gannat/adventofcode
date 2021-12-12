4a571bb66651f6d4056c98cb33407db20d598e91
Antoine GANNAT <antoine.gannat@gmail.com>
2021-12-12 12:44:16 +0100

    day12 part 1

---
+++ src/day12/part1/index.ts
@@ -0,0 +1,91 @@
+import { readInputAsLines } from "../../utils/readInput";
+
+interface ICave {
+  name: string;
+  // store the name of connections
+  connections: string[];
+  size: "big" | "small";
+}
+
+const endCaveName = "end";
+const startCaveName = "start";
+
+const data = readInputAsLines().map((line) => line.split("-"));
+
+const caves: ICave[] = [];
+
+function findCave(caveName: string): ICave | undefined {
+  return caves.find(({ name }) => name === caveName);
+}
+
+function addCave(caveName: string) {
+  if (findCave(caveName)) {
+    return;
+  }
+  caves.push({
+    name: caveName,
+    connections: [],
+    // if name uses uppercase letters, it's a big cave
+    size: caveName.charCodeAt(0) < 97 ? "big" : "small",
+  });
+}
+
+function addConnection(receivingCave: ICave, caveToAdd: ICave) {
+  // if that connection already exists, leave
+  if (receivingCave.connections.includes(caveToAdd.name)) {
+    return;
+  }
+  receivingCave.connections.push(caveToAdd.name);
+}
+
+function pathFinder(
+  entryCave: ICave,
+  visitedCaves: string[],
+  pathsFound: string[][]
+) {
+  // if the exit is found, leave
+  if (entryCave.name === endCaveName) {
+    pathsFound.push([...visitedCaves, entryCave.name]);
+    return;
+  }
+
+  // add the current cave to the visited caves
+  visitedCaves.push(entryCave.name);
+
+  // for each connection
+  for (const connection of entryCave.connections) {
+    const cave = findCave(connection);
+    // don't visit twice a small cave
+    if (
+      cave.name !== endCaveName &&
+      cave.size === "small" &&
+      visitedCaves.includes(cave.name)
+    ) {
+      continue;
+    }
+    // recursivelly visit connections
+    pathFinder(cave, [...visitedCaves], pathsFound);
+  }
+}
+
+// start by parsing the input
+
+// go through the data a first time to load all caves
+data.forEach(([caveA, caveB]) => {
+  // if the cave is not already present in the array add it
+  addCave(caveA);
+  addCave(caveB);
+});
+
+// go through the data a second time to load all connections
+data.forEach(([caveAName, caveBName]) => {
+  const caveA = findCave(caveAName);
+  const caveB = findCave(caveBName);
+  addConnection(caveA, caveB);
+  addConnection(caveB, caveA);
+});
+
+let pathsFound: string[][] = [];
+pathFinder(findCave(startCaveName), [], pathsFound);
+
+console.log("Result:", pathsFound.length);
