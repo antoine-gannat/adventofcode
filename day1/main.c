3968f456f1c1f76d65cde9adfe3c3ba537d1338a
Antoine GANNAT <angannat@microsoft.com>
2022-12-01 13:27:42 +0100

    cleanup

--- day1/main.c
+++ day1/main.c
@@ -4,11 +4,14 @@
 #include <string.h>
 
 #define BUFFER_SIZE 1024
-#define ELVES_BUFFER_SIZE 1024
+#define RAW_NUMBER_BUFFER_SIZE 10
+#define ELVES_BUFFER_SIZE 3
+#define EOL '\n'
+#define END_OF_FILE '\0'
 
 const size_t readSize = BUFFER_SIZE;
 
-char *readFile(const char *path)
+static char *readFile(const char *path)
 {
   size_t bytesRead = 0;
   size_t bufferSize = readSize;
@@ -49,74 +52,71 @@ char *readFile(const char *path)
   return buffer;
 }
 
-int *parse(char *buffer)
+// update the top elves array
+static void saveElf(int currentElfSum, int *topElves)
 {
-  int elvesIndex = 0;
-  int elfTotal = 0;
-  size_t i = 0;
-  size_t bufferIndex = 0;
-  char numberBuffer[15];
-
-  int *elves = malloc(ELVES_BUFFER_SIZE * sizeof(int));
-  if (elves == NULL)
+  // if higher than first, replace first and move the rest down
+  if (currentElfSum > topElves[0])
   {
-    return NULL;
+    topElves[2] = topElves[1];
+    topElves[1] = topElves[0];
+    topElves[0] = currentElfSum;
   }
-  memset(numberBuffer, 0, 15);
-  memset(elves, -1, ELVES_BUFFER_SIZE);
-
-  while (buffer[i] != '\0')
+  // if higher than second, replace second and move the rest down
+  else if (currentElfSum > topElves[1])
   {
-    // if end of elf
-    if (i > 0 && buffer[i - 1] == '\n' && buffer[i] == '\n')
+    topElves[2] = topElves[1];
+    topElves[1] = currentElfSum;
+  }
+  // if higher than third, replace third
+  else if (currentElfSum > topElves[2])
+  {
+    topElves[2] = currentElfSum;
+  }
+}
+
+static const int findSolution(char *buffer)
+{
+  size_t i = 0;
+  int elfTotal = 0;
+  int topElves[ELVES_BUFFER_SIZE];
+  char rawNumberBuffer[RAW_NUMBER_BUFFER_SIZE];
+
+  // init buffers
+  memset(rawNumberBuffer, 0, RAW_NUMBER_BUFFER_SIZE);
+  memset(topElves, 0, ELVES_BUFFER_SIZE);
+
+  // for each character in the file
+  while (buffer[i] != END_OF_FILE)
+  {
+    // if end of elf (current char is EOL and so is the previous one)
+    if (i > 0 && buffer[i - 1] == EOL && buffer[i] == EOL)
     {
       // save the total for this elf
-      elves[elvesIndex] = elfTotal;
-      elvesIndex++;
+      saveElf(elfTotal, topElves);
       // reset the total
       elfTotal = 0;
     }
-    else if (buffer[i] == '\n')
+    // if end of line, parse the raw number
+    else if (buffer[i] == EOL)
     {
-      int number = atoi(numberBuffer);
+      const int number = atoi(rawNumberBuffer);
+      // add the number to the total
       elfTotal += number;
-      memset(numberBuffer, 0, 15);
-      bufferIndex = 0;
+      // reset the raw number buffer
+      memset(rawNumberBuffer, 0, RAW_NUMBER_BUFFER_SIZE);
     }
     else
     {
-      numberBuffer[bufferIndex] = buffer[i];
-      bufferIndex++;
+      // concat the current char to the raw number buffer
+      strncat(rawNumberBuffer, &buffer[i], 1);
     }
     i++;
   }
   // add the last elf
-  elves[elvesIndex] = elfTotal;
-  // fill the rest with -1
-  for (int i = elvesIndex + 1; i < ELVES_BUFFER_SIZE; i++)
-  {
-    elves[i] = -1;
-  }
-  return elves;
-}
-
-// Basic sorting algorithm to sort the elves
-int *sortElves(int *arr)
-{
-  int i, j, temp;
-  for (i = 0; i < ELVES_BUFFER_SIZE; i++)
-  {
-    for (j = i + 1; j < ELVES_BUFFER_SIZE; j++)
-    {
-      if (arr[i] < arr[j])
-      {
-        temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-      }
-    }
-  }
-  return arr;
+  saveElf(elfTotal, topElves);
+  // return the sum of the top elves
+  return topElves[0] + topElves[1] + topElves[2];
 }
 
 int main()
@@ -127,11 +127,9 @@ int main()
     printf("Failed to read input file");
     return 1;
   }
-  int i = 0;
-  int *elves = sortElves(parse(fileContent));
-
-  printf("%d", elves[0] + elves[1] + elves[2]);
+  // find and print the solution
+  printf("Solution: %d\n", findSolution(fileContent));
+  // free the allocated memory
   free(fileContent);
-  free(elves);
   return 0;
 }
